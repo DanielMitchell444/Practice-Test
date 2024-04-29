76bf296cc2ca658b7e8b27f79054f4c4ea8ab355
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2024-04-29 10:23:28 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+//This file contains the practice exam for the test you will 
+//be taking on thursday. Please go through it, and if you have
+//any questions regarding any part of it, please let me know
+//and we will go over it together
+
+
+/*   
+QUESTION 1
+Below this comment, please make 3 variables.
+the first variable needs to be a integer value,
+the second variable needs to be a string value
+and the third value needs to be a boolean value
+you can name the variables whatever you want
+*/
+
+/* 
+QUESTION 2
+Inside this comment, i want you to type out the following answers to the questions
+1: What is an integer data type?
+2: What is a string data type
+3: What is a boolean data type?
+*/
+
+/*
+Type answer here
+*/
+ 
+/* 
+QUESTION 3
+Below, you will see an integer value declared with the value of 12
+below this comment, i want you to use an if else statement to check to see if
+the variable is greater than 12. If it is greater than 12, console.log("hi")
+if the variable is not greater than 12, use an else statement.
+inside the else statement, console.log("bye")
+*/
+
+let number = 12;
+
+
+/*  
+QUESTION 4
+Below this comment, i want you to create a for loop. This for loop will check to see
+if i is less than 12, and then increment the value
+inside the for loop statement, console.log("hi")
+*/
+
+
+/*
+QUESTION 5
+Below, you will see that there is a variable declared
+create a while loop that checks to see if the variable is less than 12
+*/
+
+/*
+QUESTION 6
+I want you to create a function named hello that console.log("hello")
+then, i want you to call that function.
+
+*/
+
+/*
+QUESTION 7
+Inside the index.html, you will see a h1 ID of hello,
+using getElementByID, i want you to select that h1 using the proper
+syntax
+*/
+
+/*Question 8
+Now, using the selected HTML element, i want you to change the text
+content of the h1 using .innerHTML
+*/
+
+/* Question 9
+Inside the index.html file, you will notice that there is a p tag with
+the class of hi. Using getElementByClassName, i want you to select that
+paragraph
+*/
+
+/*Question 10
+Inside the index.html file, you will notice that there are 3 buttons
+with the same class name associated with it. Using querySelectorAll, i want
+you to select the 3 buttons.
+*/
